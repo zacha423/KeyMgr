0bdc4d56f964da648b2f9c52f143a563838cc430
henr5288 <henr5288@pacificu.edu>
2023-11-05 19:16:40 -0800

    dockerfile for php complete?

--- BasicDemo/public/index.php
+++ BasicDemo/public/index.php
@@ -1,30 +1,61 @@
 <!DOCTYPE html>
+<html>
 <head>
-<title>Insert data to PostgreSQL with php - creating a simple web application</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<style>
-li {listt-style: none;}
-</style>
+    <title>Insert data to PostgreSQL with PHP - Creating a Simple Web Application</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style>
+        li {list-style: none;}
+    </style>
 </head>
 <body>
-<h2>Enter information regarding book</h2>
-<ul>
-<form name="insert" action="insert.php" method="POST" >
-<li>Book ID:</li><li><input type="text" name="bookid" /></li>
-<li>Book Name:</li><li><input type="text" name="book_name" /></li>
-<li>Author:</li><li><input type="text" name="author" /></li>
-<li>Publisher:</li><li><input type="text" name="publisher" /></li>
-<li>Date of publication:</li><li><input type="text" name="dop" /></li>
-<li>Price (USD):</li><li><input type="text" name="price" /></li>
-<li><input type="submit" /></li>
-</form>
-</ul>
+    <h2>Enter information regarding book</h2>
+    <ul>
+        <form name="insert" action="insert.php" method="POST" >
+            <li>Book ID:</li><li><input type="text" name="bookid" /></li>
+            <li>Book Name:</li><li><input type="text" name="book_name" /></li>
+            <li>Author:</li><li><input type="text" name="author" /></li>
+            <li>Publisher:</li><li><input type="text" name="publisher" /></li>
+            <li>Date of publication:</li><li><input type="text" name="dop" /></li>
+            <li>Price (USD):</li><li><input type="text" name="price" /></li>
+            <li><input type="submit" /></li>
+        </form>
+    </ul>
 </body>
 </html>
+
 <?php
-$db = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=example");
-$query = "INSERT INTO book VALUES ('$_POST[bookid]','$_POST[book_name]',
-'$_POST[author]','$_POST[publisher]','$_POST[dop]',
-'$_POST[price]')";
-$result = pg_query($query); 
+$host = "localhost";
+$port = "5432";
+$dbname = "postgres";
+$user = "postgres";
+$password = "example";
+
+$connectionString = "host=$host port=$port dbname=$dbname user=$user password=$password";
+
+// Attempt to connect to the PostgreSQL database
+$db = pg_connect($connectionString);
+
+if (!$db) {
+    die("Error: Unable to connect to the database");
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $bookid = $_POST['bookid'];
+    $book_name = $_POST['book_name'];
+    $author = $_POST['author'];
+    $publisher = $_POST['publisher'];
+    $dop = $_POST['dop'];
+    $price = $_POST['price'];
+
+    // Use a prepared statement to prevent SQL injection
+    $query = "INSERT INTO book (bookid, book_name, author, publisher, dop, price) VALUES ($1, $2, $3, $4, $5, $6)";
+    
+    $result = pg_query_params($db, $query, array($bookid, $book_name, $author, $publisher, $dop, $price));
+
+    if ($result) {
+        echo "Book information submitted successfully!";
+    } else {
+        echo "Error submitting book information.";
+    }
+}
 ?>
--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,19 @@
-# FROM php:fpm-bullseye
+FROM php:fpm-bullseye
 
-# RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
-# RUN set -ex \
-#     	&& apk --no-cache add postgresql-dev\
-#     	&& docker-php-ext-install pdo pdo_pgsql
+# Install PostgreSQL extension and other dependencies
+RUN apt-get update && apt-get install -y libpq-dev \
+    && docker-php-ext-configure pgsql -with-pgsql=/usr/include/postgresql/ \
+    && docker-php-ext-install pdo pdo_pgsql pgsql
 
-# WORKDIR /var/www/html
+# Optionally, you can install other PHP extensions or dependencies as needed.
+# For example, if you need more extensions like gd, mbstring, etc., you can add them here.
+
+# Copy your PHP application code to the container (replace '/app' with your app path)
+COPY ./BasicDemo/public var/www/html
+
+# Set your working directory
+WORKDIR /var/www/html
+
+

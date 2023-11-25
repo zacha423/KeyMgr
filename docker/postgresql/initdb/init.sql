2641df31110af9dd85fde46e0033fed7ba193347
Zachary Abela-Gale <zachabegal@gmail.com>
2023-11-25 09:09:08 +0000

    Init script creates roles.

--- docker/postgresql/initdb/init.sql
+++ docker/postgresql/initdb/init.sql
@@ -1,26 +1,26 @@
---*****************************************************************************
--- Filename:    init.sql
--- Author:      Zachary Abela-Gale
--- Date:        2023/11/24
--- Purpose:     Defines the roles and database for use by KeyManager
---*****************************************************************************
+-- ****************************************************************************
+-- Filename:init.sql
+-- Author:Zachary Abela-Gale
+-- Date:2023/11/24
+-- Purpose: Defines the roles and database for use by KeyManager
+-- ****************************************************************************
 CREATE ROLE "keymgr" WITH 
-  NOLOGIN 
-  NOINHERIT 
-  COMMENT ON ROLE "keymgr" IS "Owner of any keymgr resources.";
+	NOLOGIN 
+	NOINHERIT;
+COMMENT ON ROLE "keymgr" IS 'Owner of any keymgr resources.';
 
 CREATE ROLE "keymgr_readonly" WITH 
-  NOLOGIN
-  NOINHERIT
-  COMMENT ON ROLE "keymgr_readonly" IS "Read only role for keymgr users.";
+	NOLOGIN
+	NOINHERIT;
+COMMENT ON ROLE "keymgr_readonly" IS 'Read only role for keymgr users.';
 
 CREATE ROLE "keymgr_readwrite" WITH
-  NOLOGIN
-  NOINHERIT
-  COMMENT ON ROLE "keymgr_readwrite" IS "Read/write role for keymgr users.";
+	NOLOGIN
+	NOINHERIT;
+COMMENT ON ROLE "keymgr_readwrite" IS 'Read/write role for keymgr users.';
 
-CREATE DATABASE "keymgr" 
-  WITH OWNER = "keymgr" 
-  ENCODING = 'UTF8' 
-  CONNECTION LIMIT = -1 
-  IS_TEMPLATE = False;
+CREATE DATABASE keymgr
+	WITH OWNER = "keymgr" 
+	ENCODING = 'UTF8' 
+	CONNECTION LIMIT = -1 
+	IS_TEMPLATE = False;

47ecf40e582739cf4a8d9587b7a942febce4156e
Zachary Abela-Gale <zachabegal@gmail.com>
2023-11-25 10:06:22 +0000

    Converted tabs to spaces.

--- docker/postgresql/initdb/init.sql
+++ docker/postgresql/initdb/init.sql
@@ -5,22 +5,22 @@
 -- Purpose: Defines the roles and database for use by KeyManager
 -- ****************************************************************************
 CREATE ROLE "keymgr" WITH 
-	NOLOGIN 
-	NOINHERIT;
+  NOLOGIN 
+  NOINHERIT;
 COMMENT ON ROLE "keymgr" IS 'Owner of any keymgr resources.';
 
 CREATE ROLE "keymgr_readonly" WITH 
-	NOLOGIN
-	NOINHERIT;
+  NOLOGIN
+  NOINHERIT;
 COMMENT ON ROLE "keymgr_readonly" IS 'Read only role for keymgr users.';
 
 CREATE ROLE "keymgr_readwrite" WITH
-	NOLOGIN
-	NOINHERIT;
+  NOLOGIN
+  NOINHERIT;
 COMMENT ON ROLE "keymgr_readwrite" IS 'Read/write role for keymgr users.';
 
 CREATE DATABASE keymgr
-	WITH OWNER = "keymgr" 
-	ENCODING = 'UTF8' 
-	CONNECTION LIMIT = -1 
-	IS_TEMPLATE = False;
+  WITH OWNER = "keymgr" 
+  ENCODING = 'UTF8' 
+  CONNECTION LIMIT = -1 
+  IS_TEMPLATE = False;

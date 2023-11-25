868a351e24398cccc973b03081d4d08e4637823a
Zachary Abela-Gale <zachabegal@gmail.com>
2023-11-25 07:29:27 +0000

    Moved init scripts to new location.

---
+++ docker/postgresql/initdb/mkTables.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#psql
+#HEREDOC tags, write SQL...
+
+
+# -- GRANT ALL PRIVILEGES ON keymgr TO keymgr; 
+# -- ALTER DATABASE keymgr OWNER TO keymgr;
+
+# -- DROP TABLE book;
+
+
+
+# -- CREATE TABLE IF NOT EXISTS public.book (
+# --   book_id character varying(10) NOT NULL,
+# --   book_name character varying(50) NOT NULL,
+# --   author character varying(25),
+# --   publisher character varying(25),
+# --   date_of_publication date,
+# --   price numeric(8, 2)
+# -- );

d36880bd81a5e6cda2ef5ab5896ae509c497d059
Zachary Abela-Gale <zachabegal@gmail.com>
2023-11-26 01:57:59 +0000

    Sample table addition.

--- docker/postgresql/initdb/mkTables.sh
+++ docker/postgresql/initdb/mkTables.sh
@@ -5,23 +5,16 @@
 # Date:     2023/11/25
 # Purpose:  Add tables to the public schema for KeyMgr's database.
 ###############################################################################
+set -e
 
-#psql
-#HEREDOC tags, write SQL...
-
-
-# -- GRANT ALL PRIVILEGES ON keymgr TO keymgr; 
-# -- ALTER DATABASE keymgr OWNER TO keymgr;
-
-# -- DROP TABLE book;
-
-
-
-# -- CREATE TABLE IF NOT EXISTS public.book (
-# --   book_id character varying(10) NOT NULL,
-# --   book_name character varying(50) NOT NULL,
-# --   author character varying(25),
-# --   publisher character varying(25),
-# --   date_of_publication date,
-# --   price numeric(8, 2)
-# -- );
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "keymgr" <<-EOSQL
+  DROP TABLE IF EXISTS book;
+  CREATE TABLE IF NOT EXISTS book (
+    book_id character varying(10) NOT NULL,
+    book_name character varying(50) NOT NULL,
+    author character varying(25),
+    publisher character varying(25),
+    date_of_publication date,
+    price numeric(8, 2)
+  );
+EOSQL

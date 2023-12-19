cdc0352927dee78ae478f42e1757c9fcbf4ef845
Zachary Abela-Gale <zachabegal@gmail.com>
2023-12-19 14:37:59 +0000

    Added better documentation

--- docker/postgresql/initdb/01CreateDB.sql
+++ docker/postgresql/initdb/01CreateDB.sql
@@ -1,11 +1,13 @@
--- *****************************************************************************
+-- -----------------------------------------------------------------------------
 -- Filename:  01CreateDB.sql
 -- Authors:   Zachary Abela-Gale
 -- Date:      2023/12/17
 -- Purpose:   Create database and roles for KeyMgr.
--- *****************************************************************************
+-- -----------------------------------------------------------------------------
 
--- Create KeyMgr admin role
+-- -----------------------------------------------------------------------------
+-- Create admin access
+-- -----------------------------------------------------------------------------
 CREATE ROLE keymgr_global WITH
 	NOLOGIN     -- Only a role, not a user.
 	NOSUPERUSER -- Do not need global admin.
@@ -16,7 +18,6 @@ CREATE ROLE keymgr_global WITH
 	CONNECTION LIMIT -1;
 COMMENT ON ROLE keymgr_global IS 'Global access to KeyMgr resources.';
 
--- Create KeyMgr admin user
 CREATE ROLE keymgr_global_u WITH
 	LOGIN
 	NOSUPERUSER
@@ -29,7 +30,9 @@ CREATE ROLE keymgr_global_u WITH
 GRANT keymgr_global TO keymgr_global_u WITH ADMIN OPTION;
 COMMENT ON ROLE keymgr_global_u IS 'Admin user for all KeyMgr resources';
 
--- Create RO Role
+-- -----------------------------------------------------------------------------
+-- Create read only access
+-- -----------------------------------------------------------------------------
 CREATE ROLE keymgr_readonly WITH
 	NOLOGIN
 	NOSUPERUSER
@@ -40,18 +43,6 @@ CREATE ROLE keymgr_readonly WITH
 	CONNECTION LIMIT -1;
 COMMENT ON ROLE keymgr_readonly IS 'Role for application users that need readonly access';
 
--- Create RW ROle
-CREATE ROLE keymgr_readwrite WITH
-	NOLOGIN
-	NOSUPERUSER
-	NOCREATEDB
-	NOCREATEROLE
-	INHERIT
-	NOREPLICATION
-	CONNECTION LIMIT -1;
-COMMENT ON ROLE keymgr_readwrite IS 'Role for application users that need readwrite access.';
-
--- Create RO Web User
 CREATE ROLE keymgr_webro_u WITH
 	LOGIN
 	NOSUPERUSER
@@ -65,7 +56,19 @@ CREATE ROLE keymgr_webro_u WITH
 GRANT keymgr_readonly TO keymgr_webro_u;
 COMMENT ON ROLE keymgr_webro_u IS 'Read only user for web server connections.';
 
--- Create RW Web User
+-- -----------------------------------------------------------------------------
+-- Create read write access.
+-- -----------------------------------------------------------------------------
+CREATE ROLE keymgr_readwrite WITH
+	NOLOGIN
+	NOSUPERUSER
+	NOCREATEDB
+	NOCREATEROLE
+	INHERIT
+	NOREPLICATION
+	CONNECTION LIMIT -1;
+COMMENT ON ROLE keymgr_readwrite IS 'Role for application users that need readwrite access.';
+
 CREATE ROLE keymgr_webrw_u WITH
 	LOGIN
 	NOSUPERUSER
@@ -79,7 +82,9 @@ CREATE ROLE keymgr_webrw_u WITH
 GRANT keymgr_readwrite TO keymgr_webrw_u;
 COMMENT ON ROLE keymgr_webrw_u IS 'Read/write user for web server connections.';
 
--- Create KeyMgr DB
+-- -----------------------------------------------------------------------------
+-- Create database and assign permissions
+-- -----------------------------------------------------------------------------
 ALTER DATABASE "KeyMgr" OWNER TO keymgr_global;
 
 COMMENT ON DATABASE "KeyMgr"
@@ -91,9 +96,8 @@ GRANT CONNECT ON DATABASE "KeyMgr" TO keymgr_readonly;
 
 GRANT CONNECT ON DATABASE "KeyMgr" TO keymgr_readwrite;
 
--- Create Schema for DB
 CREATE SCHEMA "KeyMgr"
-    AUTHORIZATION keymgr_global;
+  AUTHORIZATION keymgr_global;
 
 GRANT ALL ON SCHEMA "KeyMgr" TO keymgr_global WITH GRANT OPTION;
 

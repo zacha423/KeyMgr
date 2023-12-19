c7dfdf1db676a9363fa1f29032f8cbce6889f126
Zachary Abela-Gale <zachabegal@gmail.com>
2023-12-19 14:06:44 +0000

    Country table working, inserted data via CSV using PGAdmin4

--- docker/postgresql/initdb/02CreateTables.sql
+++ docker/postgresql/initdb/02CreateTables.sql
@@ -1,12 +1,28 @@
--- Sample Table
-DROP TABLE IF EXISTS "KeyMgr".book;
-CREATE TABLE IF NOT EXISTS "KeyMgr".book (
-  book_id character varying(10) NOT NULL,
-  book_name character varying(50) NOT NULL,
-  author character varying(25),
-  publisher character varying(25),
-  date_of_publication date,
-  price numeric(8, 2)
+DROP TABLE IF EXISTS "KeyMgr"."Country";
+CREATE TABLE "KeyMgr"."Country"
+(
+  "ID" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+  iso_code3 character(3) NOT NULL,
+  name character varying(50) NOT NULL,
+  PRIMARY KEY ("ID")
 );
-ALTER TABLE IF EXISTS "KeyMgr".book
+
+ALTER TABLE IF EXISTS "KeyMgr"."Country"
+  OWNER to keymgr_global;
+
+DROP TABLE IF EXISTS "KeyMgr"."State";
+CREATE TABLE "KeyMgr"."State"
+(
+  "ID" bigint NOT NULL,
+  name character varying(50) NOT NULL,
+  abbreviation character varying(3) NOT NULL,
+  "FK_Country" bigint NOT NULL,
+  PRIMARY KEY ("ID"),
+  CONSTRAINT "FK_Country" FOREIGN KEY ("FK_Country")
+    REFERENCES "KeyMgr"."Country" ("ID") MATCH FULL
+    ON UPDATE NO ACTION
+    ON DELETE CASCADE
+);
+
+ALTER TABLE IF EXISTS "KeyMgr"."State"
   OWNER to keymgr_global;

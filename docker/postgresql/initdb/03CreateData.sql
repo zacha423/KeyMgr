5f6de9824e776044f5d87f9c5e535a6d995f26de
Zachary Abela-Gale <zachabegal@gmail.com>
2023-12-21 09:52:07 +0000

    Preparing for adding test/sample data

---
+++ docker/postgresql/initdb/03CreateData.sql
@@ -0,0 +1,38 @@
+-- Marsh Hall  2043 College Way, Forest Grove, OR 97116
+-- // Pacific University (Campus)  2043 College Way, Forest Grove, OR 97116
+-- Price Hall 2150 Cedar Street
+-- Strain Science Center 2172 Cedar Street
+-- AuCoin Hall 2125 College Way FG OR 97116
+-- Creighton 222 SE 8th Ave, Hillsboro, OR 97123
+-- HPC2 190 SE 8th Ave, Hillsboro, OR 97123
+
+-- -----------------------------------------------------------------------------
+-- Filename:  03CreateData.sql
+-- Authors:   Zachary Abela-Gale
+-- Date:      2023/12/21
+-- Purpose:   Generates sample Data for the database
+-- -----------------------------------------------------------------------------
+
+-- -----------------------------------------------------------------------------
+-- Country Table (per ISO3166)
+-- https://www.iso.org/iso-3166-country-codes.html
+-- -----------------------------------------------------------------------------
+-- Canada CAN
+-- United States of America USA
+-- Mexico MEX
+-- -----------------------------------------------------------------------------
+-- State Table
+-- -----------------------------------------------------------------------------
+-- Oregon, OR, //get US ID
+-- Washington, WA // get US ID
+-- California, CA // get US ID
+-- -----------------------------------------------------------------------------
+-- City Table
+-- -----------------------------------------------------------------------------
+-- Forest Grove
+-- Cornelius
+-- Hillsboro
+-- Seatle
+-- San Francisco
+
+-- https://www.getsynth.com/docs/blog/2021/03/09/postgres-data-gen (APACHE2)

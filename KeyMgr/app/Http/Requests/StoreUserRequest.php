15934dd0a43b81d70e21008b0be62f41bbb74843
Zachary Abela-Gale <zachabegal@gmail.com>
2024-02-02 03:42:48 +0000

    Added basic form and form request validation

--- KeyMgr/app/Http/Requests/StoreUserRequest.php
+++ KeyMgr/app/Http/Requests/StoreUserRequest.php
@@ -3,26 +3,35 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illluminate\Validation\Rules\Password;
 
 class StoreUserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;//init=false, true for test/debug
-    }
+  const PW_MIN_LEN = 8;
+  /**
+   * Determine if the user is authorized to make this request.
+   * 
+   * Because we want users to self-register an account everyone is authorized.
+   */
+  public function authorize(): bool
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'firstName' => 'required',
+      'lastName' => 'required',
+      'username' => ['required', 'unique:App\Models\User,username'],
+      // Email not yet validated to facilitate easier test accounts :)
+      'email' => ['required'/*, 'email:rfc,dns,spoof'*/],
+      'password' => ['required', 'confirmed', Password::min(self::PW_MIN_LEN)->letters()->mixedCase()->numbers()->symbols()->uncompromised()],
+    ];
+  }
 }
--- KeyMgr/resources/views/accounts/create.blade.php
+++ KeyMgr/resources/views/accounts/create.blade.php
@@ -1,7 +1,13 @@
 <h1>Accounts/Create Blade</h1>
-<p>This blade is a form to create an account. It was created using a default resource controller.</p>
+<p>This blade is a form to create an account.   
+  It was created using a default resource controller.</p>
 <form method="post" action="/accounts">
   @csrf
-  <input type="text"> test</input>
+  <input name="firstName" type="text">firstName</input>
+  <input name="lastName" type="text">lastName</input>
+  <input name="username" type="text">username</input>
+  <input name="email" type="email">email</input>
+  <input name="password" type="password">password</input>
+  <input name="password_confirmed" type="password">confirm password</input>
   <input type="submit"></input>
 </form>

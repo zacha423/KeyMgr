f960596ae068e258b3a2a57890630e05cbf052b4
Maximus <huds4450@pacificu.edu>
2024-02-12 09:31:06 +0000

    Added first/last name and username editable fields | Have not not tested

--- KeyMgr/resources/views/profile/partials/update-profile-information-form.blade.php
+++ KeyMgr/resources/views/profile/partials/update-profile-information-form.blade.php
@@ -18,9 +18,21 @@
         @method('patch')
 
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+            <x-input-label for="firstName" :value="__('First Name')" />
+            <x-text-input id="firstName" name="firstName" type="text" class="mt-1 block w-full" :value="old('firstName', $user->firstName)" required autofocus autocomplete="firstName" />
+            <x-input-error class="mt-2" :messages="$errors->get('firstName')" />
+        </div>
+
+        <div>
+            <x-input-label for="lastName" :value="__('Last Name')" />
+            <x-text-input id="lastName" name="lastName" type="text" class="mt-1 block w-full" :value="old('lastName', $user->lastName)" required autofocus autocomplete="lastName" />
+            <x-input-error class="mt-2" :messages="$errors->get('lastName')" />
+        </div>
+
+        <div>
+            <x-input-label for="username" :value="__('Username')" />
+            <x-text-input id="username" name="username" type="text" class="mt-1 block w-full" :value="old('username', $user->username)" required autofocus autocomplete="username" />
+            <x-input-error class="mt-2" :messages="$errors->get('username')" />
         </div>
 
         <div>

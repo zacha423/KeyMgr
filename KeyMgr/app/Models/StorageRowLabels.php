04eff5c752b0fc560ac044e7edb6debecb74efa2
Zachary Abela-Gale <abel1325+GH@pacificu.edu>
2024-02-13 16:29:09 +0000

    Prepared files for new key models

---
+++ KeyMgr/app/Models/StorageRowLabels.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StorageRowLabels extends Model
+{
+    use HasFactory;
+}

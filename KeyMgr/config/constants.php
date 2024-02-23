94d96013369a9fd7b33f760d85188ebaf50c7b47
Zachary Abela-Gale <zachabegal@gmail.com>
2024-02-23 08:20:38 +0000

    Added location for constants to be stored

---
+++ KeyMgr/config/constants.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Zachary Abela-Gale <abel1325@pacificu.edu>
+ * 
+ * This contains constants and strings used across many segments of KeyMgr.
+ */
+
+ return [
+  'roles' => [
+    'holder' => 'Key Holder',
+    'requestor' => 'Key Requestor',
+    'authority' => 'Key Authority',
+    'issuer' => 'Key Issuer',
+    'locksmtih' => 'Locksmith',
+    'admin' => 'Admin',
+  ]
+  ];

b402563e9d8400703a03ef7b55ee39785aee250d
Zachary Abela-Gale <zachabegal@gmail.com>
2024-02-24 02:36:47 +0000

    Added constant for group data.

--- KeyMgr/config/constants.php
+++ KeyMgr/config/constants.php
@@ -7,11 +7,15 @@
 
  return [
   'roles' => [
+    'default' => 'Key Holder',
     'holder' => 'Key Holder',
     'requestor' => 'Key Requestor',
     'authority' => 'Key Authority',
     'issuer' => 'Key Issuer',
     'locksmtih' => 'Locksmith',
     'admin' => 'Admin',
+  ],
+  'groups' => [
+    'default' => config('constants.groups.default'),
   ]
   ];
---
+++ KeyMgr/database/migrations/2024_02_24_021132_create_groups.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Zachary Abela-Gale <abel1325@pacificu.edu>
+ */
+use Illuminate\Database\Migrations\Migration;
+use App\Models\UserGroup;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    UserGroup::create(['name' => config('groups.default')]);
+  }
+};

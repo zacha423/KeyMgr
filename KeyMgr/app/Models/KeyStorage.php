c387c1a000433a5d3698dd392fc1e46299a699bc
Zachary Abela-Gale <zachabegal@gmail.com>
2024-02-25 04:12:22 +0000

    Added hook labels

--- KeyMgr/app/Models/KeyStorage.php
+++ KeyMgr/app/Models/KeyStorage.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class KeyStorage extends Model
 {
@@ -18,5 +19,15 @@ class KeyStorage extends Model
   public function room (): BelongsTo
   {
     return $this->belongsTo(Room::class);
+  }
+
+  public function columnLabels (): HasMany
+  {
+    return $this->hasMany (StorageColLabels::class);
+  }
+
+  public function rowLabels (): HasMany
+  {
+    return $this->hasMany (StorageRowLabels::class);
   }
 }
--- KeyMgr/app/Models/StorageRowLabels.php
+++ KeyMgr/app/Models/StorageRowLabels.php
@@ -1,11 +1,21 @@
 <?php
-
+/**
+ * @author Zachary Abela-Gale <abel1325@pacificu.edu>
+ */
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class StorageRowLabels extends Model
 {
-    use HasFactory;
+  protected $fillable = [
+    'rowNumber',
+    'label',
+  ];
+
+  public function keyStorage(): BelongsTo
+  {
+    return $this->belongsTo (KeyStorage::class);
+  }
 }

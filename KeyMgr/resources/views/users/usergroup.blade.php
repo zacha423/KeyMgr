b8c041e65f1c38bc8bfac6fec64a4c97e32e0229
henr5288 <henr5288@pacificu.edu>
2024-02-27 16:06:32 +0000

    deleting and adding not working, trying ajax

--- KeyMgr/resources/views/users/usergroup.blade.php
+++ KeyMgr/resources/views/users/usergroup.blade.php
@@ -1,4 +1,4 @@
-<h1>User Group Data</h1>
+<!-- <h1>User Group Data</h1>
 <form action="/groups" method="POST">
   @csrf
   <div>
@@ -26,4 +26,78 @@
 @if(isset($groupJSON))
 <h2>group/groupJSON</h2>
 {{ $groupJSON }}
-@endif
+@endif -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add or Delete Groups</title>
+</head>
+<body>
+
+<h1>Add or Delete Groups</h1>
+
+<ul id="dataList">
+    @foreach ($groups as $group)
+        <li>
+            {{ $group['name'] }}
+            <button class="deleteGroup" data-group-id="{{ $group['id'] }}">Delete</button>
+        </li>
+    @endforeach
+</ul>
+
+<input type="text" id="newGroupName" placeholder="Enter group name">
+<button onclick="addGroup()">Add Group</button>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    function addGroup() {
+        var groupName = $('#newGroupName').val();
+        if (groupName.trim() === '') {
+            alert('Please enter a group name');
+            return;
+        }
+
+        $.ajax({
+            url: '{{ route('groups.store') }}',
+            method: 'POST',
+            data: {
+                _token: '{{ csrf_token() }}',
+                name: groupName
+            },
+            success: function(response) {
+                $('#dataList').append('<li>' + groupName + '<button class="deleteGroup" data-group-id="' + response.group_id + '">Delete</button></li>');
+                $('#newGroupName').val('');
+            },
+            error: function(xhr) {
+                console.log(xhr.responseText);
+                alert('Failed to add group. Please try again.');
+            }
+        });
+    }
+
+    $(document).on('click', '.deleteGroup', function() {
+        var groupId = $(this).data('group-id');
+        var listItem = $(this).closest('li');
+
+        $.ajax({
+            url: '{{ route('groups.destroy', ['group' => $group['id']]) }}',
+            method: 'POST',
+            data: {
+                _token: '{{ csrf_token() }}',
+                id: groupId
+            },
+            success: function(response) {
+                listItem.remove();
+            },
+            error: function(xhr) {
+                console.log(xhr.responseText);
+                alert('Failed to delete group. Please try again.');
+            }
+        });
+    });
+</script>
+
+</body>
+</html>
--- KeyMgr/resources/views/users/userrole.blade.php
+++ KeyMgr/resources/views/users/userrole.blade.php
@@ -1,4 +1,4 @@
-<h1>User Role Data</h1>
+<!-- <h1>User Role Data</h1>
 <form action="/roles" method="POST">
   @csrf
   <div>
@@ -16,4 +16,82 @@
 @if(isset($rolesJSON))
 <h2>roles/rolesJSON</h2>
 {{ $rolesJSON }}
-@endif
+@endif -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add or Delete Roles</title>
+</head>
+<body>
+
+    <h1>Add or Delete Roles</h1>
+
+    <ul id="dataList">
+        @foreach ($roles as $role)
+            <li>
+                {{ $role['name'] }}
+                <button class="deleteRole" data-role-id="{{ $role['id'] }}">Delete</button>
+            </li>
+        @endforeach
+    </ul>
+
+    <input type="text" id="newRoleName" placeholder="Enter role name">
+    <button onclick="addRole()">Add Role</button>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        function addRole() {
+            var roleName = $('#newRoleName').val();
+            if (roleName.trim() === '') {
+                alert('Please enter a role name');
+                return;
+            }
+
+
+            $.ajax({
+                url: '{{ route('roles.store') }}',
+                method: 'POST',
+                data: {
+                    _token: '{{ csrf_token() }}',
+                    name: roleName
+                },
+                success: function(response) {
+                    $('#dataList').append('<li>' + roleName + '<button class="deleteRole" data-role-id="' + response.role_id + '">Delete</button></li>');
+                    $('#newRoleName').val('');
+                },
+                error: function(xhr) {
+                    console.log(xhr.responseText);
+                    alert('Failed to add role. Please try again.');
+                }
+            });
+        }
+
+        $(document).on('click', '.deleteRole', function() {
+            var roleId = $(this).data('role-id');
+            var listItem = $(this).closest('li');
+
+
+            $.ajax({
+                url: '{{ route('roles.destroy', ['role' => $role['id']]) }}',
+                method: 'POST',
+                data: {
+                    _token: '{{ csrf_token() }}',
+                    id: roleId
+                },
+                success: function(response) {
+                    listItem.remove();
+                },
+                error: function(xhr) {
+                    console.log(xhr.responseText);
+                    alert('Failed to delete role. Please try again.');
+                }
+            });
+        });
+    </script>
+
+</body>
+</html>

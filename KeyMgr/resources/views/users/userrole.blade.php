54081b583b9d0190a31fc31a24590d3dd68d8d0c
henr5288 <henr5288@pacificu.edu>
2024-03-13 05:03:22 +0000

    add and delete roles not working, but formatted a little nicer

--- KeyMgr/resources/views/users/userrole.blade.php
+++ KeyMgr/resources/views/users/userrole.blade.php
@@ -19,79 +19,91 @@
 @endif -->
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add or Delete Roles</title>
-</head>
-<body>
+<x-app-layout>
+    <x-slot name="header">
+        <div class="p-2 flex justify-between items-center">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('User Roles') }}
+            </h2>
+        </div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <ul id="dataList">
+                    @foreach ($roles as $role)
+                    <li class="flex items-center">
+                        <div class="text-gray-900 dark:text-gray-100 flex-grow mr-4">
+                            {{ $role['name'] }}
+                        </div>
+                        <button class="deleteRole btn-red" data-role-id="{{ $role['id'] }}">Delete</button>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
 
-    <h1>Add or Delete Roles</h1>
+        <input type="text" id="newRoleName" placeholder="Enter role name">
+        <button onclick="addRole()">Add Role</button>
 
-    <ul id="dataList">
-        @foreach ($roles as $role)
-            <li>
-                {{ $role['name'] }}
-                <button class="deleteRole" data-role-id="{{ $role['id'] }}">Delete</button>
-            </li>
-        @endforeach
-    </ul>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script>
+            function addRole() {
+                var roleName = $('#newRoleName').val();
+                if (roleName.trim() === '') {
+                    alert('Please enter a role name');
+                    return;
+                }
 
-    <input type="text" id="newRoleName" placeholder="Enter role name">
-    <button onclick="addRole()">Add Role</button>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        function addRole() {
-            var roleName = $('#newRoleName').val();
-            if (roleName.trim() === '') {
-                alert('Please enter a role name');
-                return;
+                $.ajax({
+                    url: '{{ route('roles.store') }}',
+                    method: 'POST',
+                    data: {
+                        _token: '{{ csrf_token() }}',
+                        name: roleName
+                    },
+                    success: function(response) {
+                        $('#dataList').append('<li class="flex items-center"><div class="text-gray-900 dark:text-gray-100 flex-grow mr-4">' + roleName + '</div><button class="deleteRole btn-red" data-role-id="' + response.role_id + '">Delete</button></li>');
+                        $('#newRoleName').val('');
+                    },
+                    error: function(xhr) {
+                        console.log(xhr.responseText);
+                        alert('Failed to add role. Please try again.');
+                    }
+                });
             }
 
+            $(document).on('click', '.deleteRole', function() {
+                var roleId = $(this).data('role-id');
+                var listItem = $(this).closest('li');
 
-            $.ajax({
-                url: '{{ route('roles.store') }}',
-                method: 'POST',
-                data: {
-                    _token: '{{ csrf_token() }}',
-                    name: roleName
-                },
-                success: function(response) {
-                    $('#dataList').append('<li>' + roleName + '<button class="deleteRole" data-role-id="' + response.role_id + '">Delete</button></li>');
-                    $('#newRoleName').val('');
-                },
-                error: function(xhr) {
-                    console.log(xhr.responseText);
-                    alert('Failed to add role. Please try again.');
-                }
+
+                $.ajax({
+                    url: '{{ route('roles.destroy', ['role' => $role['id']]) }}',
+                    method: 'POST',
+                    data: {
+                        _token: '{{ csrf_token() }}',
+                        id: roleId
+                    },
+                    success: function(response) {
+                        listItem.remove();
+                    },
+                    error: function(xhr) {
+                        console.log(xhr.responseText);
+                        alert('Failed to delete role. Please try again.');
+                    }
+                });
             });
-        }
+        </script>
 
-        $(document).on('click', '.deleteRole', function() {
-            var roleId = $(this).data('role-id');
-            var listItem = $(this).closest('li');
-
-
-            $.ajax({
-                url: '{{ route('roles.destroy', ['role' => $role['id']]) }}',
-                method: 'POST',
-                data: {
-                    _token: '{{ csrf_token() }}',
-                    id: roleId
-                },
-                success: function(response) {
-                    listItem.remove();
-                },
-                error: function(xhr) {
-                    console.log(xhr.responseText);
-                    alert('Failed to delete role. Please try again.');
-                }
-            });
-        });
-    </script>
-
-</body>
-</html>
+        <style>
+            .btn-red {
+                background-color: #e53e3e;
+                color: #fff;
+                border: none;
+                padding: 0.10rem 0.25rem;
+                border-radius: 0.5rem;
+                cursor: pointer;
+            }
+        </style>
+    </x-slot>
+</x-app-layout>

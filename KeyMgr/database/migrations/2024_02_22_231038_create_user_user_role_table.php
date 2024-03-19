96b9f1f8a708baa0b0c4c3bbddd57d030187aac8
Zachary Abela-Gale <abel1325+GH@pacificu.edu>
2024-03-19 22:14:58 +0000

    Verified/updated unique constraints

--- KeyMgr/database/migrations/2024_02_22_231038_create_user_user_role_table.php
+++ KeyMgr/database/migrations/2024_02_22_231038_create_user_user_role_table.php
@@ -1,28 +1,30 @@
 <?php
-
+/**
+ * @author Zachary Abela-Gale <abel1325@pacificu.edu>
+ */
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('user_user_role', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id');
-            $table->foreignId('user_role_id');
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('user_user_role', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('user_id');
+      $table->foreignId('user_role_id');
+      $table->unique(['user_id', 'user_role_id']);
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('user_user_role');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('user_user_role');
+  }
 };

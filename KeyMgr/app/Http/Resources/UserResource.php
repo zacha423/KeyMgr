ed7fcf7de38f706e4b5e3862a7246443b54b7689
Zachary Abela-Gale <abel1325+GH@pacificu.edu>
2024-03-19 22:16:02 +0000

    Simplified parsing of array data

--- KeyMgr/app/Http/Resources/UserResource.php
+++ KeyMgr/app/Http/Resources/UserResource.php
@@ -16,16 +16,7 @@ class UserResource extends JsonResource
    * @todo Q: Can attributes be limited based on policy or route?
    */
   public function toArray(Request $request): array
-  {
-    $roles = [];
-    $groups = [];
-    foreach ($this->roles()->get() as $role) {
-      array_push($roles, (new RoleResource($role))->name);
-    }
-    foreach ($this->groups()->get() as $group) {
-      array_push ($groups, (new GroupResource($group))->name);
-    }
-    
+  { 
     return [
       'id' => $this->id,
       'name' => $this->getFullname(),
@@ -35,8 +26,8 @@ class UserResource extends JsonResource
       'username' => $this->username,
       'created' => $this->created_at,
       'updated' => $this->updated_at,
-      'roles2' => $roles,
-      'groups2' => $groups,
+      'roles2' => array_column(RoleResource::collection($this->roles()->get())->toArray($request), 'name'),
+      'groups2' => array_column(GroupResource::collection($this->groups()->get())->toArray($request), 'name'),
     ];
   }
 }

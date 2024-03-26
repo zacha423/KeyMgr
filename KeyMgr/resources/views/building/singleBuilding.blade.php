99fc993db062d94985e934e42000ac26436bd0ac
Maximus <huds4450@pacificu.edu>
2024-03-26 06:10:01 +0000

    Added edit and delete to single building view

--- KeyMgr/resources/views/building/singleBuilding.blade.php
+++ KeyMgr/resources/views/building/singleBuilding.blade.php
@@ -3,119 +3,64 @@
 @section('title', __('Building Details'))
 
 @section('content_header')
-    <div class="p-2 flex justify-between items-center">
-        <div class="flex items-center px-2">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mr-2">
-                <span class="font-semibold text-xl">
-                    <a href="{{ route('building.index') }}" class="text-black underline">Buildings</a> 
-                    <i class="fas fa-long-arrow-alt-right text-gray-500 mx-2"></i> 
-                    {{ $building['name'] }}
-                </span>
-            </h2>
-        </div>
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Building Details</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('building.index') }}">Buildings</a></li>
+                        <li class="breadcrumb-item active">{{ $building['name'] }}</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
 @stop
 
 @section('content')
-    <div class="mb-4">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-4">
-                <div class="bg-gray-200 p-4 rounded-md flex justify-between items-center">
-                    <div>
-                        <h2 class="text-lg font-bold text-gray-800 dark:text-gray-200">Number of Rooms</h2>
-                        <p>{{ $numberOfRooms }}</p>
-                    </div>
-                    <div class="flex items-center">
-                        <div class="ml-auto">
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card card-primary card-outline">
+                        <div class="card-header">
+                            <h5 class="m-0">Number of Rooms</h5>
+                        </div>
+                        <div class="card-body">
+                            <p>{{ $numberOfRooms }}</p>
                             <a href="{{ route('building.buildingRooms',['building' => $building['id']]) }}" class="btn btn-primary">View All Rooms</a>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
+                <div class="col-lg-6">
+                    <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h5 class="m-0">Building Address</h5>
+                            <div class="btn-group">
+                                <a href="{{ route('building.edit', ['building' => $building['id']]) }}" class="btn btn-info mr-1"><i class="fas fa-edit"></i> Edit</a>
+                                <form action="{{ route('building.destroy', ['building' => $building['id']]) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger"><i class="fas fa-trash-alt"></i> Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
 
-    <div class="py-12">
-        <div class="mb-4">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-4">
-                    <div class="bg-gray-100 p-4 rounded-md">
-                        <h2 class="text-lg font-bold text-gray-800 dark:text-gray-200">Street Address</h2>
-                        @if(isset($building['streetAddress']))
-                            <p>{{ $building['streetAddress'] }}</p>
-                        @else
-                            <p>Street Address information not available</p>
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="mb-4">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-4">
-                    <div class="bg-gray-200 p-4 rounded-md">
-                        <h2 class="text-lg font-bold text-gray-800 dark:text-gray-200">Country</h2>
-                        @if(isset($building['country']))
-                            <p>{{ $building['country'] }}</p>
-                        @else
-                            <p>Country information not available</p>
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="mb-4">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-4">
-                    <div class="bg-gray-300 p-4 rounded-md">
-                        <h2 class="text-lg font-bold text-gray-800 dark:text-gray-200">State</h2>
-                        @if(isset($building['state']))
-                            <p>{{ $building['state'] }}</p>
-                        @else
-                            <p>State information not available</p>
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="mb-4">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-4">
-                    <div class="bg-gray-400 p-4 rounded-md">
-                        <h2 class="text-lg font-bold text-gray-800 dark:text-gray-200">City</h2>
-                        @if(isset($building['city']))
-                            <p>{{ $building['city'] }}</p>
-                        @else
-                            <p>City information not available</p>
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="mb-4">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-4">
-                    <div class="bg-gray-500 p-4 rounded-md">
-                        <h2 class="text-lg font-bold text-gray-800 dark:text-gray-200">Postal Code</h2>
-                        @if(isset($building['postalCode']))
-                            <p>{{ $building['postalCode'] }}</p>
-                        @else
-                            <p>Postal Code information not available</p>
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="mb-4">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-4">
-                    <div class="bg-gray-600 p-4 rounded-md">
-                        <h2 class="text-lg font-bold text-gray-800 dark:text-gray-200">Campus Location</h2>
-                        @if(isset($building['campus']))
-                            <p>{{ $building['campus'] }}</p>
-                        @else
-                            <p>Campus information not available</p>
-                        @endif
+
+                        <div class="card-body">
+                            <p><strong>Street Address:</strong> @if(isset($building['streetAddress'])){{ $building['streetAddress'] }}@else Street Address information not available @endif</p>
+                            <p><strong>Country:</strong> @if(isset($building['country'])){{ $building['country'] }}@else Country information not available @endif</p>
+                            <p><strong>State:</strong> @if(isset($building['state'])){{ $building['state'] }}@else State information not available @endif</p>
+                            <p><strong>City:</strong> @if(isset($building['city'])){{ $building['city'] }}@else City information not available @endif</p>
+                            <p><strong>Postal Code:</strong> @if(isset($building['postalCode'])){{ $building['postalCode'] }}@else Postal Code information not available @endif</p>
+                            <p><strong>Campus Location:</strong> @if(isset($building['campus'])){{ $building['campus'] }}@else Campus information not available @endif</p>
+                        </div>
+               
                     </div>
                 </div>
             </div>

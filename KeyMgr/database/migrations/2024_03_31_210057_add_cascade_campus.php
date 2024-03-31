f4235c29db10bd1ccb6b0ecea219908a0e2e4a2d
Zachary Abela-Gale <abel1325+GH@pacificu.edu>
2024-03-31 21:10:53 +0000

    Fixed missing delete cascade

---
+++ KeyMgr/database/migrations/2024_03_31_210057_add_cascade_campus.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Zachary Abela-Gale <abel1325@pacificu.edu>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::table('buildings', function (Blueprint $table) {
+      $table->dropForeign(['campus_id']);
+      $table->foreign('campus_id')->references('id')->on('campuses')->onDelete('cascade');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    //
+  }
+};

4dcda2d86788962a16756b4035d26d6970c8324e
Liam Henry <henr5288@pacificu.edu>
2024-03-31 22:20:15 +0000

    smaller buttons

--- KeyMgr/resources/views/users/userrole.blade.php
+++ KeyMgr/resources/views/users/userrole.blade.php
@@ -1,14 +1,13 @@
 
 @extends ("adminlte::page")
 @section ("content")
-
     <div class="p-2 flex justify-between items-center">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('User Roles') }}
         </h2>
     </div>
 
-    <div class="py-12">
+    <!-- <div class="py-12">
         <div class="testff max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="flex justify-between items-center">
                 <input type="text" id="newRoleName" placeholder="Enter role name" class="text-black flex-grow mr-4">
@@ -32,9 +31,62 @@
             </div>
             @endforeach
         </div>
-    </div>
+    </div> -->
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+<div class="col-12">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">All User Roles</h3>
+            <div class="card-tools">
+                <div class="input-group input-group-sm" style="width: 150px;">
+                    <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-default">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-body table-responsive p-0">
+            <table class="table table-hover text-nowrap">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Users</th>
+                    </tr>
+                </thead>
+                @foreach ($roles as $role)
+                <div class="test">
+                <tbody>
+                    <tr>
+                        <td>{{ $role['id'] }}</td>
+                        <td>{{ $role['name'] }}</td>
+                        <td>11-7-2014</td>
+                        <td>
+                            <div class="flex items-center col-sm-6">
+                            <div class="ml-auto flex items-center p-2">
+                                <button type="button" class="deleteRole btn btn-block btn-danger btn-sm" data-role-id="{{ $role['id'] }}">Delete</button>
+                            </div>
+                        </div>
+                        </td>
+                    </tr>
+                </tbody>
+                </div>
+                @endforeach
+            </table>
+        </div>
+    </div>
+</div>
+
+@stop
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         function addRole() {
             var roleName = $('#newRoleName').val();
@@ -72,10 +124,9 @@
 
         $(document).on('click', '.deleteRole', function() {
             var roleId = $(this).data('role-id');
-            var listItem = $(this).closest('.bg-white');
+            var listItem = $(this).closest('test');
 
             $.ajax({
-                // url: '{{ route('roles.destroy', ['role' => $role['id']]) }}',
                 url: '/roles/' + roleId,
                 method: 'POST',
                 data: {
@@ -83,7 +134,7 @@
                     _method: 'DELETE'
                 },
                 success: function(response) {
-                    listItem.remove();
+                    location.reload();
                 },
                 error: function(xhr) {
                     console.log(xhr.responseText);
@@ -94,14 +145,7 @@
     </script>
 
     <style>
-        .btn-red {
-            background-color: #e53e3e;
-            color: #fff;
-            border: none;
-            padding: 0.10rem 0.25rem;
-            border-radius: 0.5rem;
-            cursor: pointer;
+        .deleteRole {
+            width: 20px;
         }
     </style>
-
-@stop

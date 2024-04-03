7ee398a78a8475c76db08fadd9a9dca55220c8f7
Zachary Abela-Gale <zachabegal@gmail.com>
2024-04-03 05:51:58 +0000

    Attempt at validation

--- KeyMgr/resources/views/components/modify-membership-modal.blade.php
+++ KeyMgr/resources/views/components/modify-membership-modal.blade.php
@@ -2,12 +2,15 @@
   <x-adminlte-modal :title="$title" :id="$id" theme="info" v-centered>
     <form id="modalForm" action="{{$submitURL}}" method="post">
       @csrf
-      {{-- The name has to be different on each switch? --}}
+      
+      {{-- A basic textbox listing the number of users to operate on.  --}}
       <div class="row">
         <x-adminlte-info-box title="Selected Users" text="a" icon-theme="info" icon="fas fa-user"/>
       </div>
+
       <div class="row"><div class="col">
-        <x-adminlte-input-switch name="additionMode" label="Operation" :config="[
+        {{-- The name has to be different on each switch? --}}
+        <x-adminlte-input-switch :id="'sw'.$id" name="additionMode" label="Operation" :config="[
           'onColor' => 'green',
           'offColor' => 'red',
           'onText' => 'Add',
@@ -38,7 +41,7 @@
       </div>
     </form>
     <x-slot name="footerSlot">
-      <x-adminlte-button type="submit" class="mr-auto" label="Save" theme="success" form='modalForm'/>
+      <x-adminlte-button id="submitModal" type="submit" class="mr-auto" label="Save" theme="success" form='modalForm'/>
       <x-adminlte-button label="Cancel" theme="danger" data-dismiss='modal'/>
     </x-slot>
   </x-adminlte-modal>
@@ -54,8 +57,14 @@
       $('<select id="selectedUsers" name="selectedUsers[]" multiple hidden >').appendTo('#modalForm');
       // Load each user ID into the hidden select.
       $(()=>{getSelectedIDs('table5').forEach(($ID)=>{
-        $('<option selected />').attr('value', $ID).appendTo('#selectedUsers');
-      });});
+          $('<option selected />').attr('value', $ID).appendTo('#selectedUsers');
+        });
+      });
+
+      // Disable submit button if no users are selected.
+      $(()=> {
+        getSelectedIDs('table5').length === 0 ? $('#submitModal').attr('disabled', true):$('#submitModal').attr('disabled',false);
+      });
     });
   })
 </script>

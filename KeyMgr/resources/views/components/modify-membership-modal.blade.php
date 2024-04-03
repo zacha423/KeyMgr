e4086a95ef3068f37887fe32cb91aa8b192e86bd
Zachary Abela-Gale <zachabegal@gmail.com>
2024-04-03 07:12:31 +0000

    Group selection in modal final for now

--- KeyMgr/resources/views/components/modify-membership-modal.blade.php
+++ KeyMgr/resources/views/components/modify-membership-modal.blade.php
@@ -26,16 +26,16 @@
       </div></div>
       <div class="row">
         <div class="col">
-          <x-adminlte-select name="selectedData[]" :label="$selectName" multiple theme="info">
+          <x-adminlte-select id="modalSelector" name="selectedData[]" :label="$selectName" multiple theme="info">
             <x-slot name="prependSlot">
               <div class="input-group-text bg-gradient-info">
                 <i class="fas fa-users"></i>
               </div>
             </x-slot>
             <x-adminlte-options :options="$options"/>
-            <x-slot name="appendSlot">
-              <x-adminlte-button theme="outline-dark" label="Clear" icon="fas fa-lg fa-ban text-danger"/>
-            </x-slot>
+            {{--<x-slot name="appendSlot"> 
+              <x-adminlte-button theme="outline-dark" label="Clear" icon="fas fa-lg fa-ban text-danger"/> 
+            </x-slot>--}}
           </x-adminlte-select>
         </div>
       </div>
--- KeyMgr/resources/views/users/userlist.blade.php
+++ KeyMgr/resources/views/users/userlist.blade.php
@@ -42,7 +42,7 @@
   <x-adminlte-button type="button" theme="primary" data-toggle="modal" data-target="#roleModal" id="addRole" name="addRole" label="Manage Roles"></x-adminlte-button>
   <x-adminlte-button type="button" theme="primary" data-toggle="modal" data-target="#groupModal" id="addGroup" name="addGroup" label="Manage Groups"></x-adminlte-button>
   <x-adminlte-button type="button" theme="success" data-toggle="modal" data-target="#userForm" label="Register New User"></x-adminlte-button>
-  @include('users.partials.assignRolesModal')
+  @include('users.partials.assignRolesModal', ['options' => $roleOptions])
   @include('users.partials.assignGroupsModal', ['options' => $groupOptions])
   
   <!-- New USer Modal -->

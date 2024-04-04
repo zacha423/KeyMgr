0f863a96584d0184923afd68c3a8c31a98375ab7
Zachary Abela-Gale <zachabegal@gmail.com>
2024-04-04 08:16:36 +0000

    Added basics to results by card. Controller will need updated

--- KeyMgr/resources/views/users/usergroup.blade.php
+++ KeyMgr/resources/views/users/usergroup.blade.php
@@ -14,7 +14,10 @@
 
 {{-- Limit Search Results Card --}}
 <x-adminlte-card theme="info" theme-mode="outline" title="Limit results by:" collapsible>
-  <form></form>
+  <form>
+    <x-group-selector id="groupSelect" :options="$groupsArray" :selected="[]"></x-group-selector>
+    <x-adminlte-button type="submit" theme="primary" label="Refine Search"/>
+  </form>
 </x-adminlte-card>
 
 {{-- Tools Card --}}

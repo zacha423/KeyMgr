47eb317244e48f126c45d45dc79f4b22bec69a10
Zachary Abela-Gale <zachabegal@gmail.com>
2024-04-14 07:06:18 +0000

    Proof of concept layout

--- KeyMgr/resources/views/authorizations/partials/authTableFilter.blade.php
+++ KeyMgr/resources/views/authorizations/partials/authTableFilter.blade.php
@@ -5,17 +5,17 @@
   <div class="card-info card-outline">
   <div class="row">  
     <div class="col"><x-user-selector id="holderSel" name="holderSel" label="Key Holder" :options="[]" :selected="[]" multiple=""></x-user-selector></div>
-
+  
     <div class="col"><x-user-selector id="requestorSel" name="requestorSel[]" label="Key Requestor" :options="[]" :selected="[]" multiple="a"></x-user-selector></div>
 </div>
-</div>
+</div><br>
   {{-- Date and Quantity Filters --}}
   <div class="card-info card-outline">
   <div class="row">
     <div class="col"><x-room-selector></x-room-selector></div>
   </div>
-</div>
+</div><br>
   {{-- Location Filters --}}
-  <div class="card-info card-outline"><div class="row"><x-adminlte-card theme="info" theme-mode="outline" collapsible></x-adminlte-card></div></div>
+  <div class="card-info card-outline"><div class="row"><div class="col"><x-user-selector id="requestorSel" name="requestorSel[]" label="Key Requestor" :options="[]" :selected="[]" multiple="a"></x-user-selector></div><div class="col"><x-user-selector id="requestorSel" name="requestorSel[]" label="Key Requestor" :options="[]" :selected="[]" multiple="a"></x-user-selector></div></div></div>
 </form>
 </div>

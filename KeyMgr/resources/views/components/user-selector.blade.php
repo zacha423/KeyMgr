847694633fa96e821bee63e8cf8d620721c1a260
Zachary Abela-Gale <zachabegal@gmail.com>
2024-04-14 06:06:17 +0000

    Added user selector component

---
+++ KeyMgr/resources/views/components/user-selector.blade.php
@@ -0,0 +1,22 @@
+{{-- Basic component for selecting 1, or more, users. --}}
+<div>
+  @php
+  $selectConfig = [
+    'title' => "Select " . strtolower($label) . "...",
+    'liveSearch' => true,
+    'liveSearchPlaceholder' => 'Search...',
+    'showTick' => true,
+    'actionsBox' => true,
+  ];
+  @endphp
+
+  <x-adminlte-select-bs :id="$id" :name="$name" :label="$label" label-class="text-info" :multiple="$multiple" enable-old-support :config="$selectConfig">
+    <x-slot name="prependSlot">
+      <div class="input-group-text bg-gradiant-lightblue">
+        <i class="fas fa-user"></i>
+</div>
+</x-slot>
+
+<x-adminlte-options :options="$options" :selected="$selected"/>
+  </x-adminlte-select-bs>
+</div>

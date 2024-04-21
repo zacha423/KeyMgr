60b04834a16bd0b3f418d2a356184486299177b1
Zachary Abela-Gale <abel1325+GH@pacificu.edu>
2024-04-21 03:06:38 +0000

    Main body of the profile page complete

--- KeyMgr/resources/views/profile/edit.blade.php
+++ KeyMgr/resources/views/profile/edit.blade.php
@@ -1,4 +1,44 @@
-<x-app-layout>
+@extends('adminlte::page')
+<script src="https://code.jquery.com/jquery-3.6.0.mins.js"></script>
+@section('title', __('Profile'))
+
+@section('content_header')
+    <h1>Profile</h1>
+@stop 
+@section("content")
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <x-adminlte-profile-widget :name="$user->getFullname()"><x-adminlte-profile-col-item title="Held Keys" text=""/><x-adminlte-profile-col-item title="Outstanding Keys" text=""/><x-adminlte-profile-col-item title="Active Authorizations" text=""/></x-adminlte-profile-widget>
+        </div>
+    </div>
+     <div class="row">
+        <div class="col-sm-6">
+            <x-adminlte-card theme="info" theme-mode="outline" title="Account Information">
+                <x-adminlte-input label="Account ID" name="accountID" :value="$user->id" disabled/>
+                <x-adminlte-input label="Username" name="username" :value="$user->username" disabled/>
+                <x-adminlte-button label="Update Password" data-toggle="modal"/>
+            </x-adminlte-card >
+            @if(!empty($groups))
+            <x-adminlte-card theme="info" theme-mode="outline" title="Group Memberships" collapsible>
+                <x-list-group :elements="$groups"></x-list-group>
+            </x-adminlte-card>
+            @endif
+        </div>
+        <div class="col-sm-6">
+            <x-adminlte-card theme="info" theme-mode="outline" title="Contact Information">
+                <x-adminlte-input label="First Name" name="firstName" :value="$user->firstName" disabled/>
+                <x-adminlte-input label="Last Name" name="lastName" :value="$user->lastName" disabled/>
+                <x-adminlte-input label="Email" name="email" :value="$user->email" disabled/>
+            </x-adminlte-card>
+
+            @if(!empty($roles))
+            <x-adminlte-card theme="info" theme-mode="outline" title="Role Memberships" collapsible><x-list-group :elements="$roles"></x-list-group></x-adminlte-card>
+            @endif
+        </div>
+     </div> 
+</div>
+{{--<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Profile') }}
@@ -26,4 +66,5 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-app-layout>--}}
+@stop

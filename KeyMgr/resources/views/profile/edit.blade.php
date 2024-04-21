11b1e7090238b8bfbdcb16e4fd4d205a5f01ee7f
Zachary Abela-Gale <abel1325+GH@pacificu.edu>
2024-04-21 03:26:55 +0000

    archived original edit

--- KeyMgr/resources/views/profile/edit.blade.php
+++ KeyMgr/resources/views/profile/edit.blade.php
@@ -6,10 +6,18 @@
     <h1>Profile</h1>
 @stop 
 @section("content")
+@php
+$theme = 'lightblue';
+if($user->isElevated()){
+    $theme = "danger";
+}
+@endphp
 <div class="container-fluid">
     <div class="row">
         <div class="col">
-            <x-adminlte-profile-widget :name="$user->getFullname()"><x-adminlte-profile-col-item title="Held Keys" text=""/><x-adminlte-profile-col-item title="Outstanding Keys" text=""/><x-adminlte-profile-col-item title="Active Authorizations" text=""/></x-adminlte-profile-widget>
+            <x-adminlte-profile-widget :theme="$theme" :name="$user->getFullname()">
+                <x-adminlte-profile-row-item title="KeyMgr @ Pacific University" class="text-center border-bottom border-secondary"/>
+            </x-adminlte-profile-widget>
         </div>
     </div>
      <div class="row">
@@ -19,12 +27,14 @@
                 <x-adminlte-input label="Username" name="username" :value="$user->username" disabled/>
                 <x-adminlte-button label="Update Password" data-toggle="modal"/>
             </x-adminlte-card >
+
             @if(!empty($groups))
-            <x-adminlte-card theme="info" theme-mode="outline" title="Group Memberships" collapsible>
-                <x-list-group :elements="$groups"></x-list-group>
-            </x-adminlte-card>
+                <x-adminlte-card theme="info" theme-mode="outline" title="Group Memberships" collapsible>
+                    <x-list-group :elements="$groups"/>
+                </x-adminlte-card>
             @endif
         </div>
+
         <div class="col-sm-6">
             <x-adminlte-card theme="info" theme-mode="outline" title="Contact Information">
                 <x-adminlte-input label="First Name" name="firstName" :value="$user->firstName" disabled/>
@@ -33,38 +43,11 @@
             </x-adminlte-card>
 
             @if(!empty($roles))
-            <x-adminlte-card theme="info" theme-mode="outline" title="Role Memberships" collapsible><x-list-group :elements="$roles"></x-list-group></x-adminlte-card>
+                <x-adminlte-card theme="info" theme-mode="outline" title="Role Memberships" collapsible>
+                    <x-list-group :elements="$roles"/>
+                </x-adminlte-card>
             @endif
         </div>
      </div> 
 </div>
-{{--<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>--}}
 @stop

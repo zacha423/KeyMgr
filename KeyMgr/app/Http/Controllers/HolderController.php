cd6bd763a7beb89a49391e542ea52f971bf9876c
Maximus <huds4450@pacificu.edu>
2024-04-22 00:06:27 +0000

    Dashboard for review

--- KeyMgr/app/Http/Controllers/HolderController.php
+++ KeyMgr/app/Http/Controllers/HolderController.php
@@ -15,25 +15,20 @@ class HolderController extends Controller
     }
 
     $keysData = [];
-		$keysDue = [];
 
     foreach ($keyAuthorizations as $keyAuthorization) {
         $keys = $keyAuthorization->issuedKeys()->get();
 				foreach ($keys as $key) {
 					array_push($keysData, [
 						$key->id,
+						$key->pivot->due_date	?? 'N/A',
             $key->status->name ?? 'N/A'
-					]);
-
-					array_push($keysDue, [
-						$key->id,
-						$key->due_date	?? 'N/A'
 					]);
             
         }
     }
 
-    return view('holder.holderDash', ['keysData' => $keysData, 'users' => $user, 'keyDates' => $keysDue]);
+    return view('holder.holderDash', ['keysData' => $keysData, 'users' => $user]);
 	}
 
 }
--- KeyMgr/resources/views/holder/holderDash.blade.php
+++ KeyMgr/resources/views/holder/holderDash.blade.php
@@ -5,11 +5,62 @@
 @stop
 
 @section('plugins.Datatables', true)
+
+@php
+    use Illuminate\Support\Carbon;
+
+    $heads = [
+        'Key ID',
+        'Return Date',
+        'Key Status',
+				'Return Status',
+    ];
+
+    $processedKeyDates = [];
+    foreach ($keysData as $row) {
+        $dueDate = Carbon::parse($row[1]);
+        $daysUntilDue = Carbon::now()->diffInDays($dueDate, false);
+        
+				if ($daysUntilDue < 0) {
+            $status = 'Overdue';
+            $colorClass = 'text-danger';
+        } elseif ($daysUntilDue == 0) {
+            $status = 'Due Today';
+            $colorClass = 'text-warning';
+        } elseif ($daysUntilDue <= 7) {
+            $status = 'Due Soon';
+            $colorClass = 'text-warning';
+        } else {
+            $status = 'Upcoming';
+            $colorClass = 'text-success';
+        }
+
+        $row[] = '<span class="' . $colorClass . '">' . $status . '</span>';
+        
+        $processedKeyDates[] = $row;
+    }
+
+    $config = [
+        'data' => $processedKeyDates,
+        'order' => [[1, 'asc']],
+        'columns' => [
+            null, 
+            null,
+            ['orderable' => false],
+						null,
+        ],
+    ];
+@endphp
 @section('content')
-	<div class="container-fluid">
-		@include('holder.dueDate')
-
-		@include('holder.status')
-
-	</div>
+	<x-adminlte-card title="Users Keys" theme="info" icon="fas fa-key" collapsible>
+		<x-adminlte-datatable id="upcomingDueDatesTable" :heads="$heads" :config="$config" bordered compressed hoverable>
+				@foreach ($config['data'] as $row)
+						<tr>
+								@foreach($row as $cell)
+										<td>{!! $cell !!}</td>
+								@endforeach
+						</tr>
+				@endforeach
+		</x-adminlte-datatable>
+	</x-adminlte-card>
 @stop

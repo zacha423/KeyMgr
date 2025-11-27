30d888995c37961a0d3944e0fd674ed3e4be7135
Zachary Abela-Gale <zachabegal@gmail.com>
2025-11-27 20:04:09 +0000

    Delete button working, getting datatabes error from widget

--- KeyMgr/resources/views/campus/campusSingle.blade.php
+++ KeyMgr/resources/views/campus/campusSingle.blade.php
@@ -1,44 +1,52 @@
 @extends("adminlte::page")
 @section('title', 'Campus | ' . ($campus['name']))
 
+@php
+$crumbs=[
+  ['link' => '/locations', 'text' => 'Locations'],
+  ['link' => route('campus.index'), 'text' => 'Campuses'],
+  ['link' => '', 'text' => $campus['name']]
+];
+@endphp
+
 @section('content_header')
 <div class="row mb-2">
-  <div class="col-sm-6">
+  <div class="col-sm">
     <h1 class="m-0 text-dark">Campus Details | {{$campus['name']}}</h1>
   </div>
-  <div class="col-sm-6">
-    <ol class="breadcrumb float-sm-right">
-      <li class="breadcrumb-item"><a href="{{ route('campus.index') }}">Campuses</a></li>
-      <li class="breadcrumb-item active">{{ $campus['name'] }}</li>
-    </ol>
+  <div class="col-sm">
+    <x-breadcrumb :crumbs="$crumbs"></x-breadcrumb>
   </div>
 </div>
 @stop
 
+@section('content_top_nav_left')
+  <x-adminlte-button type="submit" theme="info" icon="fas fa-edit" label="Edit"></x-adminlte-button>
+  <x-adminlte-button type="submit" theme="danger" icon="fas fa-trash-alt" label="Delete" form="deleteCampus"></x-adminlte-button>
+  <x-adminlte-button type="submit" theme="success" label="Save"></x-adminlte-button>
+  <x-adminlte-button type="submit" theme="danger" label="Cancel"></x-adminlte-button>
+  <form id="deleteCampus" class="display:none" name="deleteCampus" method="POST" action="{{ route('campus.destroy', ['campus' => $campus['id']]) }}" onsubmit="return confirm('Are you sure about that?');">
+    @csrf
+    @method('DELETE')
+  </form>
+@endsection
+
 @section("content")
 @section('plugins.DataTables', true)
+
+
 <div class="row">
-  <div class="col-lg-6">
-    <x-adminlte-card theme="info" theme-mode="outline" title="Buildings On Campus">
-      <x-slot name="toolsSlot">
-        <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="{{ route('building.index') }}">View all Buildings</a></li>
-        </ol>
-      </x-slot>
-      @include('campus.partials.campusBuildingsDatatable')
-    </x-adminlte-card>
-  </div>
   <div class="col-lg-6">
     <x-adminlte-card theme="info" theme-mode="outline" title="Campus Information">
       <x-slot name="toolsSlot">
-      <div class="btn-group">
-            <a href="{{ route('campus.edit', ['campus' => $campus['id']]) }}" class="btn btn-info mr-1"><i class="fas fa-edit"></i> Edit</a>
-            <form action="{{ route('campus.destroy', ['campus' => $campus['id']]) }}" method="POST">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-danger"><i class="fas fa-trash-alt"></i> Delete</button>
-            </form>
-          </div>
+        <div class="btn-group">
+          <a href="{{ route('campus.edit', ['campus' => $campus['id']]) }}" class="btn btn-info mr-1"><i class="fas fa-edit"></i> Edit</a>
+          <form action="{{ route('campus.destroy', ['campus' => $campus['id']]) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger"><i class="fas fa-trash-alt"></i> Delete</button>
+          </form>
+        </div>
       </x-slot>
       <p><strong>Country:</strong> @if(($campus['country'])){{$campus['country']}}@else Country information not available @endif</p>
       <p><strong>State:</strong> @if(($campus['state'])){{$campus['state']}}@else State information not available @endif</p>
@@ -46,6 +54,27 @@
       <p><strong>Postal Code:</strong> @if(($campus['postalCode'])){{$campus['postalCode']}}@else Postal Code information not available @endif</p>
       <p><strong>Street Address:</strong> @if(($campus['streetAddress'])){{$campus['streetAddress']}}@else Street Address information not available @endif</p>
     </x-adminlte-card>
+  </div>  
+<div class="col-lg-6">
+    <x-adminlte-card theme="info" theme-mode="outline" title="Buildings On Campus">
+      <x-slot name="toolsSlot">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="{{ route('building.index') }}">View all Buildings</a></li>
+        </ol>
+      </x-slot>
+      {{--@include('campus.partials.campusBuildingsDatatable')--}}
+    </x-adminlte-card>
   </div>
+  
 </div>
+@stop
+
+@section('js')
+<script>
+  $(document).ready(function () {
+    $('button.btn-info').click(function () {
+      console.log('wazzip');
+    });
+  });
+</script>
 @stop
